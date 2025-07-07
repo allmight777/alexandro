3a6d6becae51c93b2a17063d4c29f45ca8b7a5ba
Alexandro <cocouvialexandro74@gmail.com>
2025-07-07 12:52:14 +0100

    update final9

---
+++ resources/js/admin.js
@@ -0,0 +1,11 @@
+// resources/js/admin.js
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../../public/vendors/js/vendor.bundle.base.js';
+import '../../public/vendors/js/vendor.bundle.addons.js';
+import '../../public/js/off-canvas.js';
+import '../../public/js/misc.js';
+import '../../public/js/template.js';
+import '../../public/js/dashboard.js';
+
+// Tes scripts perso
+import './popup.js';
