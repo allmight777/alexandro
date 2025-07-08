c5e22f5df20014cb7ac40b14d14d7b4d0e45ad4d
Alexandro <cocouvialexandro74@gmail.com>
2025-07-08 16:43:18 +0100

    optimize21

--- resources/views/employee/partials/footer.blade.php
+++ resources/views/employee/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="footer-wrap">
         <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">© 2025 Toolzy - Jaspe
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">© 2025 J-Tools - Jaspe
                 Technologie</span>
             <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Support technique:
                 support@toolzy.bj</span>
