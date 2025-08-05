6ac404196b6e4b353e9e004f36558527dc740a7b
allmight777 <agoliganange15@gmail.com>
2025-08-05 13:58:02 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,158 +1,101 @@
 <?php
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\EmployeController;
+
+use App\Http\Controllers\Admin\LogementController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\MaisonController;
+use App\Http\Controllers\PaiementController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Middleware\IsAdmin;
-use App\Http\Middleware\Isemp;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\BonController;
-use App\Http\Middleware\GestionnaireMiddleware;
-use Illuminate\Support\Facades\Cache;
-use App\Http\Controllers\RapportController;
-use App\Http\Controllers\GestionnaireController;
-use App\Http\Middleware\AdminOuGestionnaire;
-use Database\Seeders\AdminSeeder;
 
-Route::prefix('gestionnaire')->middleware(['auth', GestionnaireMiddleware::class])->group(function () {
-
-    Route::get('/rapports/create', [RapportController::class, 'create'])
-        ->name('gestionnaire.rapports.create')
-        ->middleware([GestionnaireMiddleware::class]);
-
-    Route::post('/rapports', [RapportController::class, 'store'])
-        ->name('gestionnaire.rapports.store')
-        ->middleware([GestionnaireMiddleware::class]);
-
-    Route::get('/rapports', [RapportController::class, 'index'])
-        ->name('gestionnaire.rapports.index')
-        ->middleware([GestionnaireMiddleware::class]);
-
-    Route::get('/rapports/{id}/download', [RapportController::class, 'download'])
-        ->name('gestionnaire.rapports.download')
-        ->middleware([GestionnaireMiddleware::class]);
-
-    Route::delete('/rapports/{rapport}', [RapportController::class, 'destroy'])->name('gestionnaire.rapports.destroy');
-    
-    Route::get('/gestionnaire/rapports/{id}', [RapportController::class, 'show'])->name('gestionnaire.rapports.show');
-
-});
-
-
+// La page welcome
 Route::get('/', function () {
-    // Cache la vue complète pendant 60 minutes
-    return Cache::remember('welcome_page', 60 * 60, function () {
-        return view('welcome')->render();  // on génère la vue puis on la met en cache
-    });
+    return view('welcome');
+})->name('welcome');
+
+// La routes des contacts
+Route::get('/contacts', function () {
+    return view('contacts.contact');
+})->name('contact');
+
+// la routes des agents
+Route::get('/agents', function () {
+    return view('contacts.agent');
+})->name('agent');
+
+// la route des services
+Route::get('/services', function () {
+    return view('contacts.service');
+})->name('service');
+
+// la route des terms
+Route::get('/terms', function () {
+    return view('contacts.terms');
+})->name('terms');
+
+// La route pour about us
+Route::get('/abouts', function () {
+    return view('contacts.abouts');
+})->name('abouts');
+
+// la route privacy
+Route::get('/privacy', function () {
+    return view('contacts.privacy');
+})->name('privacy');
+
+// La route pour les proprietes
+Route::get('/logements', [MaisonController::class, 'index'])->name('maison');
+// show details
+Route::get('/logements/{id}', [MaisonController::class, 'show'])->name('logements.show');
+
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+
+    // route pour gerer les users
+    Route::get('/admin/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/admin/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/admin/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/admin/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+    // route ressource pour gérer les logements
+    Route::resource('logements', LogementController::class);
+    Route::get('admin/logements/{logement}', [LogementController::class, 'show'])->name('logements.show');
+
+    // route pour gestion des demandes
+    Route::get('/paiements', [PaiementController::class, 'index'])->name('paiements.index');
+    Route::post('/paiements/{paiement}/update-status', [PaiementController::class, 'updateStatus'])->name('paiements.updateStatus');
+
 });
-
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [AdminController::class,"ShowHomePage"])->name('admin.homedash')->middleware([AdminOuGestionnaire::class]);
-    Route::get('/dashboard/employe', [EmployeController::class, "index"])->name('dashboard.employee')->middleware([Isemp::class]);
-});
-
-Route::get('/redirect-by-role', function () {
-    $role = Auth::user()->role;
-    return match ($role) {
-        'admin','gestionnaire' => redirect('/dashboard'),
-        'employé' => redirect('/dashboard/employe'),
-
-    };
-})->middleware(['auth'])->name('verifylogin');
 
 Route::middleware('auth')->group(function () {
+
+    // Details appartement
+
+    Route::get('/logements/{id}', [MaisonController::class, 'show'])->name('logements.show');
+
+    // edit
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // vue pour ceux qui sont pas admin
+
+    Route::get('/users', function () {
+        return view('users');
+    })->name('users');
+
+    // Route de paiment
+    Route::get('/paiement', [PaiementController::class, 'lancer'])->name('paiement.lancer');
+    Route::post('/paiement/initier', [PaiementController::class, 'initierPaiement'])->name('paiement.initier');
+    Route::post('/paiement/confirmer', [PaiementController::class, 'confirmerPaiement'])->name('paiement.confirmer');
+    Route::get('/paiement/confirmation/{id}', [PaiementController::class, 'confirmation'])->name('paiement.confirmation');
+
+    // Suivi des demandes de l'utilisateurs
+
+    Route::get('/mes-paiements', [PaiementController::class, 'mesPaiements'])->name('mes.paiements');
 });
 
-//Admin ou Gestionaire
-Route::prefix("dashboard")->middleware(['auth', AdminOuGestionnaire::class])->group(function () {
-    Route::get("/add_tool", [AdminController::class, "addToolpage"])
-        ->name('addToolpage');
-    Route::post("/add-tool_post", [AdminController::class, "addTool"])
-        ->name('addTool');
-    Route::get('/list_equip', [AdminController::class, "ShowToolpage"])
-        ->name("ShowToolpage");
-    Route::get('/put_tool_page/{equipement}', [AdminController::class, "putToolpage"])
-        ->name("putToolpage");
-    Route::put('/put_tool/{equipement}', [AdminController::class, "putTool"])
-        ->name("putTool");
-    Route::get("/delete_tool/{equipement}", [AdminController::class, "DeleteTool"])
-        ->name("DeleteTool");
-    Route::get("/demandes_list", [AdminController::class, "ShowAllAsk"])
-        ->name("liste.demandes");
-    Route::put("/check_demande/{demande}", [AdminController::class, "CheckAsk"])
-        ->name("valider.demande");
-    Route::put("/cancel_demande/{demande}", [AdminController::class, "CancelAsk"])
-        ->name("refuser.demande");
-    Route::put("/loading_demandes,{demande}",[AdminController::class,"LoadingAsk"])
-         ->name("loading.demande");
-    Route::get('/affectation', [AdminController::class, "Showaffectation"])
-        ->name('page.affectation');
-    Route::post('/afectation-post', [AdminController::class, "HandleAffectation"])
-        ->name("handle.affectation");
-    Route::get("/listes_affectations",[AdminController::class,"Showlistaffectation"])
-          ->name("page.listeAffectations");
-    Route::get('/equipement-pannes',[AdminController::class,"Showpannes"])
-         ->name("equipements.pannes");
-    Route::put('/pannes_modify/{panne}',[AdminController::class,"PutPanne"])
-          ->name('pannes.resolu');
-    Route::get("/list_tools_lost",[AdminController::class,"ShowToollost"])
-         ->name("tools.lost");
-    Route::get("/add-collaborateur-page",[AdminController::class,"CollaboratorsPage"])
-          ->name("CollaboratorsPage");
-    Route::post("/collaborator_submit",[AdminController::class,"HandleCollaborator"])
-         ->name("HandleCollaborator");
-    Route::get('/list_collaborator',[AdminController::class,"ShowListCollaborator"])
-        ->name("ShowListCollaborator");
-    Route::delete("/delete_collaborator/{CollaborateurExterne}",[AdminController::class,"destroy"])
-         ->name("collaborateurs.destroy");
-    Route::get('/list_bon',[AdminController::class,"ShowBons"])
-         ->name("liste.bons");
-    Route::get('/bon_collaborator_external',[AdminController::class,"CreateBon"])
-        ->name("CreateBon");
-    Route::post("/post_bon_collaborator_external",[AdminController::class,"HandleBon"])
-         ->name("HandleBon");
-    Route::post("/back_tool/{affectation}",[AdminController::class,"BackTool"])
-           ->name("affectation.retourner");
+require __DIR__.'/auth.php';
 
-});
-Route::prefix("dashboard")->middleware(['auth',IsAdmin::class])->group(function(){
-     Route::get('/list_users', [AdminController::class, "showusers"])
-        ->name("showusers");
-    Route::get('/edituser/{user}', [AdminController::class, "edituserpage"])
-        ->name('edituser')
-        ->middleware(['auth', IsAdmin::class]);
-    Route::get('deleteuser/{user}', [AdminController::class, "deleteuser"])
-        ->name('deleteuser')
-        ->middleware(['auth', IsAdmin::class]);
-    Route::put('editusers/{user}', [AdminController::class, "ModifyUser"])
-        ->name('putuser');
-     Route::get("/list_rapport",[AdminController::class,"ShowRapport"])
-         ->name("rapport.lists");
-});
-
-Route::prefix("employee")->middleware(['auth', Isemp::class])->group(function () {
-
-    Route::get('/demande-equipement', [EmployeController::class, 'ShowAskpage'])->name('demande.equipement');
-    Route::post("/demande-equipement-soumise", [EmployeController::class, "SubmitAsk"])->name("demande.soumise");
-    Route::get('/signaler-panne', [EmployeController::class, 'signalerPanne'])->name('signaler.panne');
-    Route::post("/post-signaler-panne",[EmployeController::class,"HandlePanne"])->name("post.HandlePanne");
-    // //liaison implicite
-    // Route::delete('/delete_panne/{panne}',[EmployeController::class,"DeletePanne"])->name("delete.panne");
-    //liaison implicite
-    Route::get('/equipements-assignes', [EmployeController::class, 'equipementsAssignes'])->name('equipements.assignes');
-    Route::get("/help-employee",[EmployeController::class,"Helppage"])
-         ->name("page.aide");
-    Route::post("/post-aide",[EmployeController::class,"HandleHelp"])
-         ->name("send.aide");
-    Route::delete("/delete_ask/{demande}",[EmployeController::class,"DeleteAsk"])->name("delete.ask");
-    Route::get('/panne_listes',[EmployeController::class,"ShowPannes"])->name('historique.pannes');
-    Route::get('/demandes_list',[EmployeController::class,"ShowDemandes"])->name("listes.demandes");
-        
-});
-
-//deleteuser
-require __DIR__ . '/auth.php';
